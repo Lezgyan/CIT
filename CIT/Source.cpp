b70422bfb99218bb0957196632837ad210d195c6
Lezgyan Andrew <lezgyanandrew@gmail.com>
2023-04-04 20:56:56 +0400

    Fix the program so that the sequence of numbers starts with the values 0, 1

--- CIT/Source.cpp
+++ CIT/Source.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int fib(int n) {
-	int k = 0;
-	if (n == 0) {
+	if (n == 0 || n == 1) {
 		return 0;
 	}
-	if (n == 1 || n == 2) {
+	if (n == 2) {
 		return 1;
 	}
 	return fib(n - 1) + fib(n - 2);
 }
+
 int main() {
 	int n; cin >> n;
 	cout << fib(n);
